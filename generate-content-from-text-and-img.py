f4ad5e60f0062c6055c21a10a7c18a13d199ab92
yymasaki <y.masaki34@icloud.com>
2025-02-16 13:52:07 +0900

    first-commit

---
+++ generate-content-from-text-and-img.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+import settings
+from google import genai
+from PIL import Image
+
+GEMINI_API_KEY = settings.AP
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+image = Image.open("./img/cat.jpg")
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[image, "Tell me about this cat"])
+print(response.text)
---
+++ settings.py
@@ -0,0 +1,9 @@
+# coding: UTF-8
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+AP = os.environ.get("GEMINI_API_KEY")
