250bb7ceb46768ffa8f31a873c3c5e803bcdccd9
yymasaki <y.masaki34@icloud.com>
2025-02-17 15:20:55 +0900

    入力文言を日本語に変更したら日本語のレスポンスが返ってくる

--- generate-content-from-text-and-img.py
+++ generate-content-from-text-and-img.py
@@ -10,5 +10,5 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 image = Image.open("./img/cat.jpg")
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=[image, "Tell me about this cat"])
+    contents=[image, "この猫について教えてください"])
 print(response.text)
